74ec4cc61c0af6fb3761229970b6a163979257aa
mediafoks <mediafoks@gmail.com>
2024-02-08 21:44:28 +0300

    Russian language added

--- ksmeta.php
+++ ksmeta.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @version    1.0.6
+ * @version    1.0.7
  * @package    ksmeta (plugin)
  * @author     Sergey Kuznetsov - mediafoks@google.com
  * @copyright  Copyright (c) 2024 Sergey Kuznetsov
