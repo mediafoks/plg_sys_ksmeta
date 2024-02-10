d584a73f4f64afaf1db53db731410b3ac13c7e43
mediafoks <mediafoks@gmail.com>
2024-02-10 10:53:00 +0300

    Plugin structure updated for Joomla 5

--- src/Extension/KsMeta.php
+++ src/Extension/KsMeta.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @version    1.1.1
+ * @version    1.12
  * @package    ksmeta (plugin)
  * @author     Sergey Kuznetsov - mediafoks@google.com
  * @copyright  Copyright (c) 2024 Sergey Kuznetsov
