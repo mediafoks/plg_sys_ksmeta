e519c9a04c147ca1e5b6b8c4b6125356e98c579d
mediafoks <mediafoks@gmail.com>
2024-09-04 07:40:28 +0300

    language update

--- services/provider.php
+++ services/provider.php
@@ -27,7 +27,7 @@ return new class() implements ServiceProviderInterface
      *
      * @return  void
      *
-     * @since   1.3.0
+     * @since   1.3.1
      */
     public function register(Container $container)
     {
